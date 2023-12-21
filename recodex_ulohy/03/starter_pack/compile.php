813f4e5073009421a56f69c4dccf477b26fb3240
Martin Lejko <Martin.lejko@outlook.com>
2023-12-21 11:17:29 +0100

    Vela zleho

--- recodex_ulohy/03/starter_pack/compile.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-
-require_once __DIR__ . '/templator.php';
-
-$templator = new Templator();
-$templator->loadTemplate('./template.tpl.html');
-$templator->compileAndSave('./template.php');
--- recodex_ulohy/05/debug/parameters/results/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-	'order' => [ 'name', 'points', 'memes' ],
-	'desc' => 'int'
-];
