fd327a8ffbb32bc2f85a22929b2e2731c99ae536
Martin Lejko <Martin.lejko@outlook.com>
2023-12-23 19:26:36 +0100

    Initial line sweeping

--- recodex_ulohy/09/rectangles.py
+++ recodex_ulohy/09/rectangles.py
@@ -1,51 +1,48 @@
-# "width": 600,
-# 	"height": 400,
-# 	"rects": [
-# 		{ "left": 20, "top": 10, "width": 150, "height": 30 },
-# 		{ "left": 500, "top": 50, "width": 40, "height": 300 },
-# 		{ "left": 290, "top": 300, "width": 20, "height": 100 },
-# 		{ "left": 50, "top": 270, "width": 80, "height": 80 }
-# 	]
+def maxFreeRect(width, height, rects):
+    events = []
 
-width = 600
-height = 400
-rectangles = [
-    {"left": 20, "top": 10, "width": 150, "height": 30},
-    {"left": 500, "top": 50, "width": 40, "height": 300},
-    {"left": 290, "top": 300, "width": 20, "height": 100},
-    {"left": 50, "top": 270, "width": 80, "height": 80}
-]
+    for rect in rects:
+        events.append((rect['left'], True, rect))
+        events.append((rect['left'] + rect['width'], False, rect))
 
-def checkIntersection(x, y, x1, y1):
-    for rect in rectangles:
-        rect_left = rect['left']
-        rect_top = rect['top']
-        rect_width = rect['width']
-        rect_height = rect['height']
-        
-        rect_right = rect_left + rect_width
-        rect_bottom = rect_top + rect_height
-        
-        if not (x1 < rect_left or x > rect_right or y1 < rect_top or y > rect_bottom):
-            return True  
-    
-    return False  
+    events.sort(key=lambda x: (x[0], not x[1]))
 
+    maxRect = {
+        'top': 0,
+        'left': 0,
+        'width': 0,
+        'height': 0
+    }
 
-def maxFreeRectangles(height, width, rectangles):
-    maxRect = {'top': -1, 'left': -1, 'widthR': 0, 'heightR': 0}
+    active_rects = set()
 
-    for x in range(width):
-        for y in range(height):
-            for x1 in range(x + 1, width + 1):
-                for y1 in range(y + 1, height + 1):
-                    if not checkIntersection(x, y, x1, y1):
-                        currWidth = x1
-                        currHeight = y1
-                        
-                        if currWidth * currHeight > maxRect['widthR'] * maxRect['heightR']:
-                            maxRect = {'top': y, 'left': x, 'widthR': currWidth, 'heightR': currHeight}
+    prev_x = 0
+
+    for x, is_start, rect in events:
+        interval_width = x - prev_x
+
+        max_height = 0
+        for active_rect in active_rects:
+            max_height = max(max_height, active_rect['top'] + active_rect['height'])
+
+        potential_rect = {
+            'left': prev_x,
+            'top': 0,
+            'width': interval_width,
+            'height': max_height
+        }
+
+        potential_area = potential_rect['width'] * potential_rect['height']
+        max_area = maxRect['width'] * maxRect['height']
+
+        if potential_area > max_area:
+            maxRect = potential_rect
+
+        if is_start:
+            active_rects.add(rect)
+        else:
+            active_rects.remove(rect)
+
+        prev_x = x
 
     return maxRect
-
-print(maxFreeRectangles(height, width, rectangles))
